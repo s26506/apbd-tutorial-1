a8d63ea3213c472bc9fc0874564d6be567b4653e
s26506 <s26506@pjwstk.edu.pl>
2024-03-11 13:33:14 +0100

    Dodano metode srednia

--- zajecia1/zajecia1/Program.cs
+++ zajecia1/zajecia1/Program.cs
@@ -6,5 +6,16 @@
         Console.WriteLine("Druga");
         Console.WriteLine("3");
     }
+
+    public static float Srednia(int[] t)
+    {
+        float wynik = 0;
+        foreach(var elem in t)
+        {
+            wynik += elem;
+        }
+
+        return wynik / t.Length;
+    }
 }
 
