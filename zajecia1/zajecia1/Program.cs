bcb3631f9d8c0e33c3842e7a5cc1d68e34af6039
Bartosz Bazyluk <s26506@pjwstk.edu.pl>
2024-03-17 20:49:21 +0100

    Dodalem metode Maksymalna

--- zajecia1/zajecia1/Program.cs
+++ zajecia1/zajecia1/Program.cs
@@ -14,8 +14,19 @@
         {
             wynik += elem;
         }
-
         return wynik / t.Length;
+    }
+
+    public static int Maksymalna(int[] t)
+    {
+        int m = 0;
+        for (int i = 0; i < t.Length; i++)
+        {
+            if (i == 0) m = t[i];
+            else
+            if (m < t[i]) m = t[i];
+        }
+        return m;
     }
 }
 
